8e35ff9894da99145a7b6b6bc57af5c57f557734
Tim Butorac <timbutorac@gmail.com>
2024-06-07 11:43:29 -0700

    handle not found

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -36,8 +43,14 @@ export class MessagesController {
   // getMessage() is the method that will be called when a GET request is made to the /messages/:id route.
   @Get(':id')
   // The `@Param` decorator is used to extract the value of a route parameter.
-  getMessage(@Param('id') id: string) {
+  async getMessage(@Param('id') id: string) {
     console.log('id', id);
-    return this.messagesService.findOne(id);
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+
+    return message;
   }
 }
